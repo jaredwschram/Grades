f87d55936d99d15772893759e5bde448d4aa69f1
Jared Schram <jschram@web.web.com>
2018-02-01 07:00:18 -0500

    Add project files.

---
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Tests.Types
+{
+    [TestClass]
+    public class TypeTests
+    {
+        [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+            Assert.AreEqual(69.9f, grades[2]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+           grades[1] = 89.1f;
+           grades[2] = 69.9f;
+        }
+
+        [TestMethod]
+        public void UpperCaseString()
+        {
+            string name = "jared";
+            name = name.ToUpper();
+
+            Assert.AreEqual("JARED", name);
+        }
+
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+            DateTime date = new DateTime(1988, 4, 11);
+            date = date.AddDays(-9);
+
+            Assert.AreEqual(2, date.Day);
+        }
+
+        [TestMethod]
+        public void ValueTypesPassByValue()
+        {
+            int x = 46;
+            IncrementNumber(x);
+            Assert.AreEqual(46, x);
+        }
+
+        private void IncrementNumber(int number)
+        {
+            number += 1;
+        }
+        [TestMethod]
+        public void ReferenceTypesPassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+            GiveBookAName(book2);
+            Assert.AreEqual("A GradeBook", book1.Name);
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            book.Name = "A GradeBook";
+        }
+
+        [TestMethod]
+        public void StringComparisons()
+        {
+            string name1 = "jared";
+            string name2 = "Jared";
+
+            bool result = string.Equals(name1, name2, StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IntVariablesHoldaValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+            Assert.AreNotEqual(x1, x2);
+        }
+
+        [TestMethod]
+        public void GradeBookVariablesHoldaReference()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1.Name = "Test Grade Book";
+            Assert.AreEqual(g1.Name, g2.Name);
+        }
+    }
+}
---
+++ Grades/NameChangedEventsArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class NameChangedEventsArgs : EventArgs
+    {
+        public string ExistingName { get; set; }
+        public string NewName { get; set; }
+    }
+}
